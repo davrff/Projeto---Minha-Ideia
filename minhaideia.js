14edf6a8f874b539d64e216cdb526ba2600d17e8
Davi Rodrigues <davirodrigues123407@gmail.com>
2026-05-03 22:54:32 -0300

    Formatando o codigo de javascript

--- minhaideia.js
+++ minhaideia.js
@@ -1,4 +1,4 @@
-// codigo para aparecer os links no mobile 
+// codigo para aparecer os links no mobile
 
 const botaoMenu = document.getElementById("botao_links_mobile");
 const iconesMenu = botaoMenu.querySelector("i");
@@ -6,16 +6,16 @@ const cabecalhoPagina = document.getElementById("cabecalho_pagina");
 const menuPagina = document.getElementById("menu_pagina");
 
 botaoMenu.addEventListener("click", () => {
-    menuPagina.classList.toggle("ativo");
-    cabecalhoPagina.classList.toggle("menu_aberto");
+  menuPagina.classList.toggle("ativo");
+  cabecalhoPagina.classList.toggle("menu_aberto");
 
-    if (menuPagina.classList.contains("ativo")) {
-        iconesMenu.classList.remove("fa-bars");
-        iconesMenu.classList.add("fa-x");
-    } else {
-       iconesMenu.classList.remove("fa-x");
-        iconesMenu.classList.add("fa-bars");
-    }
+  if (menuPagina.classList.contains("ativo")) {
+    iconesMenu.classList.remove("fa-bars");
+    iconesMenu.classList.add("fa-x");
+  } else {
+    iconesMenu.classList.remove("fa-x");
+    iconesMenu.classList.add("fa-bars");
+  }
 });
 
 // codigo para abrir e fechar o modal
@@ -25,11 +25,11 @@ const modal = document.getElementById("modal");
 const botaoFecharModal = document.getElementById("botao_fechar_modal");
 
 function abrirModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function fecharModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 botaoAbrirModal.addEventListener("click", abrirModal);
@@ -38,40 +38,44 @@ botaoFecharModal.addEventListener("click", fecharModal);
 
 // codigo para mudar de pagina pelo botão #botao-voltar-pagina-inicial
 
-const botaoVoltarPaginaInicial = document.getElementById("botao_voltar_pagina_inicial");
+const botaoVoltarPaginaInicial = document.getElementById(
+  "botao_voltar_pagina_inicial",
+);
 
- if (botaoVoltarPaginaInicial) {
-    botaoVoltarPaginaInicial.addEventListener("click", () => {
-        window.location.href = "index.html";
-    });
- }
+if (botaoVoltarPaginaInicial) {
+  botaoVoltarPaginaInicial.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+}
 
 // validação do formulário
 
 const formularioPagina = document.getElementById("formulario");
 
 formularioPagina.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-const inputIdeia = document.getElementById("input_modal_ideia").value.trim();
-const inputDesejo =document.getElementById("input_modal_desejo").value.trim();
+  const inputIdeia = document.getElementById("input_modal_ideia").value.trim();
+  const inputDesejo = document
+    .getElementById("input_modal_desejo")
+    .value.trim();
 
-if (inputIdeia === "" || inputDesejo === "") {
+  if (inputIdeia === "" || inputDesejo === "") {
     alert("Por favor, preencha todos os campos do formulário.");
     return;
-}
+  }
 
-let ideiasSalvas = JSON.parse(localStorage.getItem("ideiasSalvas")) || [];
+  let ideiasSalvas = JSON.parse(localStorage.getItem("ideiasSalvas")) || [];
 
-const novaIdeia = {
+  const novaIdeia = {
     ideia: inputIdeia,
-    desejo: inputDesejo
-}
+    desejo: inputDesejo,
+  };
 
-ideiasSalvas.push(novaIdeia);
+  ideiasSalvas.push(novaIdeia);
 
-localStorage.setItem("ideiasSalvas", JSON.stringify(ideiasSalvas));
+  localStorage.setItem("ideiasSalvas", JSON.stringify(ideiasSalvas));
 
-alert("Ideia enviada com sucesso!");
-formularioPagina.reset();
+  alert("Ideia enviada com sucesso!");
+  formularioPagina.reset();
 });
